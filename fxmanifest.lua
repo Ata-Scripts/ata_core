539521b0459b66e67241bbc21d98368a5111d64a
Ata Esmaeilpour <47323005+atarevals@users.noreply.github.com>
2025-03-11 07:11:44 +0330

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'ATA REVALS - https://github.com/atarevals'
+description 'All Rights Reserved - ata.tebex.io'
+version '1.0.1'
+lua54 'yes'
+
+ui_page 'html/index.html'
+
+
+files {
+    'html/**'
+}
+
+shared_scripts {
+	'shared/*.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
+
+
+escrow_ignore {
+
+}
